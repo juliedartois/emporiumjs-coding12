7d42294f0599fb7fd44d8c73f5575266adfefe0b
julie <julie.dartois@icloud.com>
2020-02-13 10:28:34 +0100

    Modal Reponsive

--- public/js/main.js
+++ public/js/main.js
@@ -2,29 +2,37 @@
 let logoNav = document.getElementsByTagName('h1')[1];
 let nav = document.getElementsByTagName('nav')[0];
 let beige = document.getElementsByClassName('beige')[0];
+let menu = document.getElementsByClassName('navbar-toggler')[0];
+let menuDrop = document.getElementsByClassName('collapse')[0];
 
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 190) {
         logoNav.classList.remove('d-none');
         nav.classList.add('fixed-top');
-        nav.style.borderBottom = '2px lightgray solid'
-        beige.style.marginTop = '158px'
+        nav.style.borderBottom = '2px lightgray solid';
+        beige.style.marginTop = '158px';
     } else if (window.scrollY < 190) {
         logoNav.classList.add('d-none');
         nav.classList.remove('fixed-top');
-        nav.style.border = '0px'
-        beige.style.marginTop = '0px'
+        nav.style.border = '0px';
+        beige.style.marginTop = '0px';
     };
+});
+
+menu.addEventListener('click', () => {
+    menuDrop.style.zIndex = '1'
 });
 
 // // MODAL
 let connect = document.getElementsByClassName('btn-secondary')[0];
 let modal = document.getElementById('modal');
 let body = document.getElementsByTagName('body')[0];
+modal.style.maxHeight = `${window.innerHeight - nav.offsetHeight}`;
 
 connect.addEventListener('click', () => {
     modal.classList.remove('d-none');
-    body.style.zIndex = '-1'
+    body.style.zIndex = '-2';
+    nav.classList.add('d-none')
 });
 
 // Sign In
@@ -33,7 +41,9 @@ let logInForm = document.getElementsByClassName('logInForm')[0];
 
 logIn.addEventListener('click', () => {
     logInForm.classList.remove('d-none');
-    signUpForm.classList.add('d-none')
+    logInForm.classList.add('d-flex');
+    signUpForm.classList.add('d-none');
+    signUpForm.classList.remove('d-flex');
 });
 
 // Sign Up
@@ -42,5 +52,25 @@ let signUpForm = document.getElementsByClassName('signUpForm')[0];
 
 signUp.addEventListener('click', () => {
     signUpForm.classList.remove('d-none');
+    signUpForm.classList.add('d-flex');
     logInForm.classList.add('d-none');
+    logInForm.classList.remove('d-flex');
+});
+
+// Close
+let cross1 = document.getElementsByClassName('fa-times')[0];
+let cross2 = document.getElementsByClassName('fa-times')[1];
+
+cross1.addEventListener('click', () => {
+    modal.classList.remove('d-block');
+    modal.classList.add('d-none');
+    body.style.zIndex = '0';
+    nav.classList.remove('d-none')
+});
+
+cross2.addEventListener('click', () => {
+    modal.classList.remove('d-block');
+    modal.classList.add('d-none');
+    body.style.zIndex = '0';
+    nav.classList.remove('d-none')
 });
